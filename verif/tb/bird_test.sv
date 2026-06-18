fa45acef935f7499490d7140f9c970ca5016c319
Ibrahim Al Ardah <ardah04@gmail.com>
2026-06-19 02:18:30 +0300

    Added first 4 tests selections

--- verif/tb/bird_test.sv
+++ verif/tb/bird_test.sv
@@ -6,24 +6,45 @@ module bird_test(bird_if vif);
   string test_name;
 
   initial begin
-    local_basic_test local_test;
-
     if (!$value$plusargs("TEST=%s", test_name)) begin
-      test_name = "local_basic_test";
+      test_name = "reset_clears_all_outputs_test";
     end
 
     case (test_name)
-      "local_basic_test": begin
-        local_test = new(vif);
-        test = local_test;
+
+      "reset_clears_all_outputs_test": begin
+        reset_clears_all_outputs_test t;
+        t = new(vif);
+        test = t;
+      end
+
+      "valid_ready_basic_transfer_test": begin
+        valid_ready_basic_transfer_test t;
+        t = new(vif);
+        test = t;
+      end
+
+      "backpressure_stability_test": begin
+        backpressure_stability_test t;
+        t = new(vif);
+        test = t;
+      end
+
+      "cfg_sampled_on_first_payload_byte_test": begin
+        cfg_sampled_on_first_payload_byte_test t;
+        t = new(vif);
+        test = t;
       end
 
       default: begin
-        $fatal(1, "Unknown TEST=%s", test_name);
+        $display("ERROR: Unknown TEST=%s", test_name);
+        $finish;
       end
+
     endcase
 
     test.run();
+    $finish;
   end
 
 endmodule
