f230968449186222eaa69f07ff565e8811e24d4a
Karim Dwikat <karimdwikat@MacBook-Air-Karim.local>
2026-06-18 21:17:24 +0300

    Modified

--- verif/tests/negative_tests.sv
+++ verif/tests/negative_tests.sv
@@ -1,6 +1,21 @@
-.
 
 
+// TP12 - invalid cfg drops
+class invalid_cfg_test extends bird_base_test;
+  function new(virtual bird_if vif);
+    super.new(vif, "invalid_cfg_test");
+  endfunction
+
+  virtual task run();
+    bird_invalid_cfg_sequence seq;
+    super.run();
+    seq = new();
+    seq.body(env);
+    repeat (200) @(posedge vif.clk);
+    report();
+    $finish;
+  endtask
+endclass
 
 
 // TP13 - remote protocol error drops
