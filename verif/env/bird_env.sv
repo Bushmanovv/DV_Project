3e5a39b40ab0c962367ad3abbdb1444a97f7687a
Ibrahim Al Ardah <ardah04@gmail.com>
2026-06-21 23:19:14 +0300

    Update bird_env.sv

--- verif/env/bird_env.sv
+++ verif/env/bird_env.sv
@@ -2,32 +2,42 @@ class bird_env;
 
   virtual bird_if vif;
 
-  bird_input_agent    input_agent;
-  bird_local_monitor  local_mon;
+  bird_input_agent   input_agent;
+  bird_local_monitor local_mon;
   bird_remote_monitor remote_mon;
-  bird_local_checker  local_checker;
+  bird_local_checker local_checker;
   bird_remote_checker remote_checker;
+  bird_drop_checker drop_checker;
+  bird_coverage coverage;
 
   mailbox #(bird_transaction) input_obs_mbx;
+  mailbox #(byte unsigned) local_byte_mbx;
+  mailbox #(bit [31:0]) remote_word_mbx;
   mailbox #(bird_transaction) remote_obs_mbx;
-  mailbox #(byte unsigned)    local_byte_mbx;
-  mailbox #(bit [31:0])       remote_word_mbx;
+  mailbox #(bird_transaction) drop_mbx;
+  mailbox #(bird_transaction) cov_mbx;
 
   function new(virtual bird_if vif);
     this.vif = vif;
 
-    input_obs_mbx  = new();
-    remote_obs_mbx = new();
-    local_byte_mbx = new();
+    input_obs_mbx   = new();
+    local_byte_mbx  = new();
     remote_word_mbx = new();
+    remote_obs_mbx  = new();
+    drop_mbx        = new();
+    cov_mbx         = new();
 
     input_agent = new(vif, input_obs_mbx);
+    local_mon = new(vif, local_byte_mbx);
+    remote_mon = new(vif, remote_word_mbx);
+    local_checker = new(input_obs_mbx, local_byte_mbx);
     input_agent.mon.add_subscriber(remote_obs_mbx);
+    input_agent.mon.add_subscriber(drop_mbx);
+    input_agent.mon.add_subscriber(cov_mbx);
 
-    local_mon      = new(vif, local_byte_mbx);
-    remote_mon     = new(vif, remote_word_mbx);
-    local_checker  = new(input_obs_mbx, local_byte_mbx);
     remote_checker = new(remote_obs_mbx, remote_word_mbx);
+    drop_checker = new(drop_mbx, vif);
+    coverage     = new(cov_mbx);
   endfunction
 
   task run();
@@ -37,12 +47,16 @@ class bird_env;
       remote_mon.run();
       local_checker.run();
       remote_checker.run();
+      drop_checker.run();
+      coverage.run();
     join_none
   endtask
 
   function void report();
     local_checker.report();
     remote_checker.report();
+    drop_checker.report();
+    coverage.report();
   endfunction
 
 endclass
