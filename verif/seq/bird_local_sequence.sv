37c999373d7e7005ddbd9b54a466fa9f83516278
m4en <171984179+M4EN@users.noreply.github.com>
2026-06-21 21:14:21 +0300

    updated local sequence

--- verif/seq/bird_local_sequence.sv
+++ verif/seq/bird_local_sequence.sv
@@ -7,13 +7,17 @@ class bird_local_basic_sequence extends bird_base_sequence;
     packet_count = 1;
   endfunction
 
-  virtual task body(bird_env env);
+  virtual task body();
     bird_transaction tr;
 
     for (int unsigned i = 0; i < packet_count; i++) begin
       tr = new($sformatf("local_packet_%0d", i));
-      tr.set_local_packet(1, 5'd1);
-      env.input_agent.send(tr);
+      if (!tr.randomize() with {
+        cfg_obj.traffic_type == 1'b0;
+        cfg_obj.frag_num     == 5'd1;
+        cfg_obj.seq_num      == 5'd1;
+      }) $fatal(1, "[%s] randomize failed", name);
+      gen.transmit(tr);
       $display("[%0t] SEQ sent: %s", $time, tr.sprint());
     end
   endtask
