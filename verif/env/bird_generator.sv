827d2e8d82f9c69bc5ef89f199869b05f3e74fec
m4en <171984179+M4EN@users.noreply.github.com>
2026-06-21 21:11:48 +0300

    updated generator

--- verif/env/bird_generator.sv
+++ verif/env/bird_generator.sv
@@ -1,23 +1,13 @@
 class bird_generator;
 
   mailbox #(bird_transaction) gen2drv_mbx;
-  int unsigned num_transactions;
 
   function new(mailbox #(bird_transaction) gen2drv_mbx);
     this.gen2drv_mbx = gen2drv_mbx;
-    num_transactions = 1;
   endfunction
 
-  task run();
-    bird_transaction tr;
-
-    repeat (num_transactions) begin
-      tr = new("generated_local_transaction");
-      tr.set_local_packet(4, 5'd1);
-      gen2drv_mbx.put(tr);
-      $display("[%0t] GEN sent: %s", $time, tr.sprint());
-    end
+  task transmit(bird_transaction tr);
+    gen2drv_mbx.put(tr);
   endtask
 
 endclass
-
