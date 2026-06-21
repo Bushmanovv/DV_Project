6b37b51983d9843a89309b1733054198a03f97b3
Ibrahim Al Ardah <ardah04@gmail.com>
2026-06-21 23:22:13 +0300

    Updated bird_pkg.sv

--- verif/env/bird_pkg.sv
+++ verif/env/bird_pkg.sv
@@ -14,10 +14,14 @@ package bird_pkg;
   `include "bird_remote_monitor.sv"
   `include "bird_local_checker.sv"
   `include "bird_remote_checker.sv"
+  `include "bird_drop_checker.sv"
+  `include "bird_coverage.sv"
   `include "bird_agent.sv"
   `include "bird_env.sv"
   `include "bird_base_sequence.sv"
   `include "bird_local_sequence.sv"
+  `include "bird_remote_sequence.sv"
+  `include "bird_negative_sequence.sv"
   `include "bird_base_test.sv"
   `include "reset_clears_all_outputs_test.sv"
   `include "valid_ready_basic_transfer_test.sv"
@@ -25,6 +29,7 @@ package bird_pkg;
   `include "cfg_sampled_on_first_payload_byte_test.sv"
   `include "local_basic_test.sv"
   `include "local_payload_boundary_test.sv"
-  `include "local_backpressure_test.sv"
+  `include "remote_tests.sv"
+  `include "negative_tests.sv"
 
 endpackage
